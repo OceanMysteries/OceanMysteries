bd09b99bd4de22f2bcce3e0e989b5e5372df1838
OceanMysteries <155460866+OceanMysteries@users.noreply.github.com>
2024-03-02 21:57:36 +0100

    Update remerciement.js

--- Javascript/remerciement.js
+++ Javascript/remerciement.js
@@ -1,9 +1,6 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyHAFKHCMEwqaeRxP3iz9eBuv0yJu4DxFVDAno6juxKz3zRanlgFAKppSjr3t6OXioO2w/exec';
 const Rform = document.forms['remerciement-form'];
 
-const participationSelect = document.getElementById("participation");
-        const selectedValue = participationSelect.value;
-
 Rform.addEventListener('submit', e => {
   e.preventDefault();
 
@@ -12,7 +9,7 @@ Rform.addEventListener('submit', e => {
   formData.append('formType', 'form2');
 
   fetch(scriptURL, { method: 'POST', body: formData })
-    .then(response => alert("Thank you! Your form is submitted successfully."))
+    .then(response => alert("Merci! Votre formulaire a été soumis avec succès."))
     .then(() => { window.location.reload(); })
-    .catch(error => console.error('Error!', error.message));
+    .catch(error => console.error('Erreur !', error.message));
 });
